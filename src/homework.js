07471f09e158a78dee5491f4ac8fcadbc442da8c
Mauricio Dayler Muñoz <miaut16umf@gmail.com>
2024-03-23 12:04:12 -0400

    add first question

--- src/homework.js
+++ src/homework.js
@@ -5,16 +5,23 @@
   /* First excercise */
   
   let numbers = [1, 2, 3, 4, 5];
+  let multiplied = numbers.map(num => num * 5);
   
+  console.log(multiplied); 
   /* Multiplique los numeros del arreglo por 5 */
 
   /*Resultado esperado: [5, 10, 15, 20, 25] */
 
   /* First excercise */
 
+
+
   /* Second excercise */
 
   let names = ["Jhosep", "Daniel", "Rodrigo", "Veronica"];
+  let sortedNames = names.sort();
+
+  console.log(sortedNames);
 
   /* Ordernar los nombres alfabeticamente */
 
@@ -22,19 +29,31 @@
 
   /* Second excercise */
 
+
+
   /* Thrid excercise */
 
   const data = ['a', 'b', 'z', 'r', 'a', 'c', 'r', 'a'];
+  let uniqueData = [...new Set(data)];
 
+  console.log(uniqueData);
   /* Dado un arreglo, crear otro sin letras repetidas */
   /* Resultado esperado: ['a', 'b', 'z', 'r', 'c'] */
 
   /* Thrid excercise */
 
+
+
   /* Fourth excercise */
 
   const largeWord = "abcdabcdABCD";
+  let frequency = {};
 
+      for (let char of largeWord) {
+        frequency[char] = (frequency[char] || 0) + 1;
+      }
+
+        console.log(frequency); 
   /* Dado un string mostrar cuantas veces se repite cada letra*/
   /* Resultado: 
     {
